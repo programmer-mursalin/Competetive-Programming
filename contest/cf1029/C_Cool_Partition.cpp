22cbd29b8ab01b10f373ccae9f1722d705c81032
programmer-mursalin <programmer.mursalin016@gmail.com>
2025-06-09 23:45:51 +0600

    codeforcesRound1029

---
+++ contest/cf1029/C_Cool_Partition.cpp
@@ -0,0 +1,79 @@
+#include <bits/stdc++.h>
+// #include <ext/pb_ds/assoc_container.hpp>
+// #include <ext/pb_ds/tree_policy.hpp>
+using namespace std;
+// using namespace __gnu_pbds;
+#define int long long
+#define gcd __gcd
+
+#define ALL(x) (x).begin(), (x).end()
+#define py cout << "YES\n";
+#define pm cout << "-1\n";
+#define pz cout << "0\n";
+#define pn cout << "NO\n";
+#define cheakmate return;
+const int N = 1e5 + 5;
+#define Mod 1000000009 + 7
+
+void solve()
+{
+
+    // 2d input
+    // vector<vector< int>> d(n, vector< int>(m));
+    int n;
+    cin >> n;
+    vector<int> a(n);
+    for (int i = 0; i < n; i++)
+    {
+        cin >> a[i];
+    }
+    set<int> st1, st2;
+    st1.insert(a[0]);
+    int cnt = 0;
+    int ans = 1;
+    map<int, int> mp;
+    for (int i = 1; i < n; i++)
+    {
+
+        st2.insert(a[i]);
+
+        if (st1.count(a[i]) && mp[a[i]] == 0)
+        {
+            cnt++;
+            mp[a[i]] = 1;
+        }
+        if (cnt == st1.size() && cnt != 0)
+        {
+            ans++;
+            st1 = st2;
+            st2.clear();
+            cnt = 0;
+            mp.clear();
+        }
+    }
+
+    cout << ans << endl;
+}
+// sort(ALL(a),greater<int>());
+// int maxi=*max_element(a.begin(),a.end());
+//  int maxi = distance(a.begin(), max_element(a.begin(), a.end()));   // return max index
+
+signed main()
+{
+
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        solve();
+    }
+    return 0;
+}
+
+/*===============================================
+  :::::::::::::Author :Md.Mursalin:::::::::::::
+  ===============================================*/
