f8799b76366abf00c2611e0fd0bac4b5aeb624b1
programmer-mursalin <programmer.mursalin016@gmail.com>
2025-06-12 03:38:29 +0600

    math

---
+++ 1400/L_Bridge_Renovation.cpp
@@ -0,0 +1,57 @@
+#include <bits/stdc++.h>
+// #include <ext/pb_ds/assoc_container.hpp>
+// #include <ext/pb_ds/tree_policy.hpp>
+using namespace std;
+// using namespace __gnu_pbds;
+#define int long long
+#define gcd __gcd
+
+#define ALL(x) (x).begin(), (x).end()
+#define py cout << "YES\n";
+#define pm cout << "-1\n";
+#define pz cout << "0\n";
+#define pn cout << "NO\n";
+#define cheakmate return;
+const int N = 1e5 + 5;
+#define Mod 1000000009 + 7
+
+void solve()
+{
+
+    // 2d input
+    // vector<vector< int>> d(n, vector< int>(m));
+    int n;
+    cin >> n;
+    // vector< int> a(n);
+    // for (int i = 0; i < n; i++)
+    // {
+    //     cin >> a[i];
+    //}
+    if (n & 1)
+    {
+        cout << ceil(n / 2.0) * 2 + ceil(ceil(n / 2.0) / 3.0) - 1 << endl;
+    }
+    else
+    {
+        cout << 2 * ceil(n / 2.0) + ceil(ceil(n / 2.0) / 3.0) << endl;
+    }
+}
+// sort(ALL(a),greater<int>());
+// int maxi=*max_element(a.begin(),a.end());
+//  int maxi = distance(a.begin(), max_element(a.begin(), a.end()));   // return max index
+
+signed main()
+{
+
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+
+    solve();
+
+    return 0;
+}
+
+/*===============================================
+  :::::::::::::Author :Md.Mursalin:::::::::::::
+  ===============================================*/
