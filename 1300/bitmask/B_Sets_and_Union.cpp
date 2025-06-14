4d64deedb7538b39632cb7ee70c3a10d5e62ccd4
programmer-mursalin <programmer.mursalin016@gmail.com>
2025-06-14 18:11:36 +0600

    bitmasking

---
+++ 1300/bitmask/B_Sets_and_Union.cpp
@@ -0,0 +1,116 @@
+#include <bits/stdc++.h>
+// #include <ext/pb_ds/assoc_container.hpp>
+// #include <ext/pb_ds/tree_policy.hpp>
+using namespace std;
+// using namespace __gnu_pbds;
+#define int long long
+#define gcd __gcd
+
+#define ALL(x) (x).begin(), (x).end()
+#define py cout << "YES\n";
+#define pm cout << "-1\n";
+#define pz cout << "0\n";
+#define pn cout << "NO\n";
+#define cheakmate return;
+const int N = 1e5 + 5;
+#define Mod 1000000009 + 7
+
+void solve()
+{
+
+    // 2d input
+    // vector<vector< int>> d(n, vector< int>(m));
+    int n;
+    cin >> n;
+
+    set<int> st;
+    vector<set<int>> vst;
+    for (int i = 0; i < n; i++)
+    {
+
+        set<int> s;
+        int m;
+        cin >> m;
+        for (int j = 0; j < m; j++)
+        {
+            int x;
+            cin >> x;
+            st.insert(x);
+            s.insert(x);
+        }
+        // for (int it : s)
+        // {
+        //     mp[it] += (s.size() - 1);
+        // }
+        vst.push_back(s);
+        s.clear();
+    }
+    size_t maxi = 0;
+
+    for (int it1 : st)
+    {
+        set<int> ans;
+        set<int> c;
+        set<int> b;
+        // maxi = max(maxi, (st.size() - mp[it]));
+
+        for (int i = 0; i < vst.size(); i++)
+        {
+            // cout << "Set " << i + 1 << ": ";
+
+            b = vst[i];
+            int cnt = 0;
+            for (int it2 : b)
+            {
+
+                if (it2 == it1)
+                {
+
+                    for (int k = 0; k < vst.size(); k++)
+                    {
+
+                        c = vst[k];
+
+                        if (c.count(it2) == 0)
+                        {
+                            for (int it4 : c)
+                            {
+                                ans.insert(it4);
+                            }
+                        }
+
+                        // cout << endl;
+                    }
+                }
+            }
+        }
+        maxi = max(maxi, ans.size());
+        ans.clear();
+        b.clear();
+        c.clear();
+    }
+    cout << maxi << endl;
+}
+// sort(ALL(a),greater<int>());
+// int maxi=*max_element(a.begin(),a.end());
+//  int maxi = distance(a.begin(), max_element(a.begin(), a.end()));   // return max index
+
+signed main()
+{
+
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        solve();
+    }
+    return 0;
+}
+
+/*===============================================
+  :::::::::::::Author :Md.Mursalin:::::::::::::
+  ===============================================*/
