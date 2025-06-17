6b8e816eca4156709f6babaac42faa5b7f56666c
programmer-mursalin <programmer.mursalin016@gmail.com>
2025-06-17 19:53:37 +0600

    binary_serach

--- 1400/bit-mask/E_Iva_Pav.cpp
+++ 1400/bit-mask/E_Iva_Pav.cpp
@@ -1,32 +1,33 @@
-#include <bits/stdc++.h>
+#include <iostream>
+#include <vector>
 using namespace std;
 
-#define int long long
-
-const int MAX_BITS = 32;
-
+// Function to solve each test case
 void solve()
 {
     int n;
-    cin >> n;
-    vector<int> a(n);
+    cin >> n; // Number of elements
 
-    for (int i = 0; i < n; ++i)
+    vector<int> a(n); // Array
+    for (int i = 0; i < n; i++)
     {
-        cin >> a[i];
+        cin >> a[i]; // Input elements
     }
 
-    // Preprocessing: bitCount[bit][i] = how many numbers till i have bit `bit` set
-    vector<vector<int>> bitCount(MAX_BITS, vector<int>(n, 0));
+    // Preprocessing for each bit position
+    vector<vector<int>> bitCount(32, vector<int>(n, 0));
 
-    for (int bit = 0; bit < MAX_BITS; ++bit)
+    for (int bit = 0; bit < 32; bit++)
     {
-        for (int i = 0; i < n; ++i)
+        for (int i = 0; i < n; i++)
         {
-            if (a[i] & (1LL << bit))
+            // Check if the 'bit'-th bit is set in a[i]
+            if ((a[i] & (1 << bit)) == 0)
             {
                 bitCount[bit][i] = 1;
             }
+
+            // Add previous value to make prefix sum
             if (i > 0)
             {
                 bitCount[bit][i] += bitCount[bit][i - 1];
@@ -35,35 +36,48 @@ void solve()
     }
 
     int q;
-    cin >> q;
+    cin >> q; // Number of queries
 
     while (q--)
     {
         int l, k;
-        cin >> l >> k;
-        l--; // 0-based indexing
+        cin >> l >> k; // 1-based index
+        l--;           // Convert to 0-based index
 
-        int low = l, high = n - 1;
-        int answer = -1;
+        int low = l;
+        int high = n - 1;
+        int answer = -1; // If no valid r found
 
         while (low <= high)
         {
             int mid = (low + high) / 2;
-            int val = 0;
+            int andValue = 0;
 
-            for (int bit = 0; bit < MAX_BITS; ++bit)
+            for (int bit = 0; bit < 32; bit++)
             {
-                int count = bitCount[bit][mid] - (l > 0 ? bitCount[bit][l - 1] : 0);
-                if (count == mid - l + 1)
+                int count;
+
+                if (l > 0)
                 {
-                    val |= (1LL << bit);
+                    count = bitCount[bit][mid] - bitCount[bit][l - 1];
+                }
+                else
+                {
+                    count = bitCount[bit][mid];
+                }
+
+                int length = mid - l + 1;
+
+                if (count == 0)
+                {
+                    andValue = andValue | (1 << bit);
                 }
             }
 
-            if (val >= k)
+            if (andValue >= k)
             {
-                answer = mid;
-                low = mid + 1;
+                answer = mid;  // Update answer
+                low = mid + 1; // Try for bigger range
             }
             else
             {
@@ -71,25 +85,28 @@ void solve()
             }
         }
 
+        // If no answer found, print -1
         if (answer == -1)
+        {
             cout << -1 << " ";
+        }
         else
-            cout << (answer + 1) << " "; // convert back to 1-based index
+        {
+            cout << (answer + 1) << " "; // Convert back to 1-based index
+        }
     }
 
-    cout << "\n";
+    cout << endl;
 }
 
-int32_t main()
+int main()
 {
-    ios_base::sync_with_stdio(false);
-    cin.tie(nullptr);
-
     int t;
-    cin >> t;
+    cin >> t; // Number of test cases
+
     while (t--)
     {
-        solve();
+        solve(); // Solve each test case
     }
 
     return 0;
