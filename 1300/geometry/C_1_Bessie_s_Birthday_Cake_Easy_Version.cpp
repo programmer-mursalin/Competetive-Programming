360394696f69bfef3a2b67fd62e0fba2a765d08e
programmer-mursalin <programmer.mursalin016@gmail.com>
2025-07-12 11:10:23 +0600

    math

---
+++ 1300/geometry/C_1_Bessie_s_Birthday_Cake_Easy_Version.cpp
@@ -0,0 +1,72 @@
+#include <bits/stdc++.h>
+// #include <ext/pb_ds/assoc_container.hpp>
+// #include <ext/pb_ds/tree_policy.hpp>
+using namespace std;
+// using namespace __gnu_pbds;
+#define int long long
+#define gcd __gcd
+
+#define ALL(x) (x).begin(), (x).end()
+#define py cout << "YES\n";
+#define pm cout << "-1\n";
+#define pz cout << "0\n";
+#define pn cout << "NO\n";
+#define cheakmate return;
+const int N = 1e5 + 5;
+#define Mod 1000000009 + 7
+
+void solve()
+{
+
+    // 2d input
+    // vector<vector< int>> d(n, vector< int>(m));
+    //   int n;
+    // cin >> n;
+    // vector< int> a(n);
+    // for (int i = 0; i < n; i++)
+    // {
+    //     cin >> a[i];
+    //}
+    int n, x, y;
+    cin >> n >> x >> y;
+
+    vector<int> a(x);
+    for (int i = 0; i < x; i++)
+        cin >> a[i], a[i]--;
+
+    set<int> st(a.begin(), a.end());
+    int ans = x - 2;
+
+    for (int i = 0; i < x; i++)
+    {
+        int t1 = (a[i] + 1) % n;
+        int t2 = (a[i] + 2) % n;
+        if (!st.count(t1) && st.count(t2))
+            ans++;
+    }
+
+    cout << ans << endl;
+}
+// sort(ALL(a),greater<int>());
+// int maxi=*max_element(a.begin(),a.end());
+//  int maxi = distance(a.begin(), max_element(a.begin(), a.end()));   // return max index
+
+signed main()
+{
+
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        solve();
+    }
+    return 0;
+}
+
+/*===============================================
+  :::::::::::::Author :Md.Mursalin:::::::::::::
+  ===============================================*/
