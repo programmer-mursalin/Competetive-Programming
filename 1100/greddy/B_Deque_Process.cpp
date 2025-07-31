410537a71f5fe6779bf5f7700a6bab2aaa297029
programmer-mursalin <programmer.mursalin016@gmail.com>
2025-07-31 23:11:43 +0600

    greedy

---
+++ 1100/greddy/B_Deque_Process.cpp
@@ -0,0 +1,105 @@
+#include <bits/stdc++.h>
+// #include <ext/pb_ds/assoc_container.hpp>
+// #include <ext/pb_ds/tree_policy.hpp>
+using namespace std;
+// using namespace __gnu_pbds;
+#define int long long
+#define gcd __gcd
+
+#define ALL(x) (x).begin(), (x).end()
+#define py cout << "YES\n";
+#define pm cout << "-1\n";
+#define pz cout << "0\n";
+#define pn cout << "NO\n";
+#define cheakmate return;
+const int N = 1e5 + 5;
+#define Mod 1000000009 + 7
+
+void solve()
+{
+
+    // 2d input
+    // vector<vector< int>> d(n, vector< int>(m));
+    int n;
+    cin >> n;
+    vector<int> a(n);
+    for (int i = 0; i < n; i++)
+    {
+        cin >> a[i];
+    }
+    cout << "L";
+    int curr = a[0];
+    int i = 1, j = n - 1, ok = 1;
+    while (i < j)
+    {
+        if (curr < a[i] && curr < a[j])
+        {
+
+            if (a[i] < a[j])
+            {
+                cout << "R" << "L";
+                curr = a[i];
+            }
+            else
+            {
+                cout << "L" << "R";
+                curr = a[j];
+            }
+        }
+
+        else if (curr > a[i] && curr > a[j])
+        {
+
+            if (a[i] < a[j])
+            {
+                cout << "L" << "R";
+                curr = a[j];
+            }
+            else
+            {
+                cout << "R" << "L";
+                curr = a[i];
+            }
+        }
+
+        else
+        {
+            cout << "L" << "R";
+            curr = a[j];
+        }
+
+        i++;
+        j--;
+
+        if (i == j)
+        {
+            cout << "L";
+            cout << endl;
+            cheakmate
+        }
+    }
+    cout << endl;
+}
+// sort(ALL(a),greater<int>());
+// int maxi=*max_element(a.begin(),a.end());
+//  int maxi = distance(a.begin(), max_element(a.begin(), a.end()));   // return max index
+
+signed main()
+{
+
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        solve();
+    }
+    return 0;
+}
+
+/*===============================================
+  :::::::::::::Author :Md.Mursalin:::::::::::::
+  ===============================================*/
---
+++ 1100/greddy/C_Count_Good_Numbers.cpp
@@ -0,0 +1,73 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define int long long
+#define gcd __gcd
+
+#define ALL(x) (x).begin(), (x).end()
+#define py cout << "YES\n";
+#define pm cout << "-1\n";
+#define pz cout << "0\n";
+#define pn cout << "NO\n";
+#define cheakmate return;
+const int N = 1e5 + 5;
+#define Mod 1000000009 + 7
+
+int lcm(int a, int b)
+{
+    return (a / gcd(a, b)) * b;
+}
+
+void solve()
+{
+    int l, r;
+    cin >> l >> r;
+
+    int total = r - l + 1;
+
+    int A = r / 2 - (l - 1) / 2;
+    int B = r / 3 - (l - 1) / 3;
+    int C = r / 5 - (l - 1) / 5;
+    int D = r / 7 - (l - 1) / 7;
+
+    int AB = r / lcm(2, 3) - (l - 1) / lcm(2, 3);
+    int AC = r / lcm(2, 5) - (l - 1) / lcm(2, 5);
+    int AD = r / lcm(2, 7) - (l - 1) / lcm(2, 7);
+    int BC = r / lcm(3, 5) - (l - 1) / lcm(3, 5);
+    int BD = r / lcm(3, 7) - (l - 1) / lcm(3, 7);
+    int CD = r / lcm(5, 7) - (l - 1) / lcm(5, 7);
+
+    int ABC = r / lcm(lcm(2, 3), 5) - (l - 1) / lcm(lcm(2, 3), 5);
+    int ABD = r / lcm(lcm(2, 3), 7) - (l - 1) / lcm(lcm(2, 3), 7);
+    int ACD = r / lcm(lcm(2, 5), 7) - (l - 1) / lcm(lcm(2, 5), 7);
+    int BCD = r / lcm(lcm(3, 5), 7) - (l - 1) / lcm(lcm(3, 5), 7);
+
+    int ABCD = r / lcm(lcm(lcm(2, 3), 5), 7) - (l - 1) / lcm(lcm(lcm(2, 3), 5), 7);
+
+    int divisible =
+        (A + B + C + D) - (AB + AC + AD + BC + BD + CD) + (ABC + ABD + ACD + BCD) - ABCD;
+
+    int not_divisible = total - divisible;
+
+    cout << not_divisible << "\n";
+}
+
+signed main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        solve();
+    }
+
+    return 0;
+}
+
+/*===============================================
+  :::::::::::::Author : Md. Mursalin ::::::::::::
+  ===============================================*/
