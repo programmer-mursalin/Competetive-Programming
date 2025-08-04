fa404203fbf3b95fd45291a418a9d07fe0913b06
programmer-mursalin <programmer.mursalin016@gmail.com>
2025-08-04 23:33:07 +0600

    data_structure

---
+++ 1100/greddy/D_Stay_or_Mirror.cpp
@@ -0,0 +1,61 @@
+#include <bits/stdc++.h>
+// #include <ext/pb_ds/assoc_container.hpp>
+// #include <ext/pb_ds/tree_policy.hpp>
+using namespace std;
+// using namespace __gnu_pbds;
+#define int long long
+#define gcd __gcd
+
+#define ALL(x) (x).begin(), (x).end()
+#define py cout << "YES\n";
+#define pm cout << "-1\n";
+#define pz cout << "0\n";
+#define pn cout << "NO\n";
+#define cheakmate return;
+const int N = 1e5 + 5;
+#define Mod 1000000009 + 7
+
+void solve()
+{
+
+    int n;
+    cin >> n;
+    deque<int> dq(n);
+    for (int i = 0; i < n; ++i)
+    {
+        cin >> dq[i];
+    }
+
+    int ans = 0;
+    for (int i = 1; i <= n; ++i)
+    {
+        int pos = find(ALL(dq), i) - dq.begin();
+        ans += min(pos, (int)dq.size() - pos - 1);
+        dq.erase(dq.begin() + pos);
+    }
+
+    cout << ans << endl;
+}
+// sort(ALL(a),greater<int>());
+// int maxi=*max_element(a.begin(),a.end());
+//  int maxi = distance(a.begin(), max_element(a.begin(), a.end()));   // return max index
+
+signed main()
+{
+
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        solve();
+    }
+    return 0;
+}
+
+/*===============================================
+  :::::::::::::Author :Md.Mursalin:::::::::::::
+  ===============================================*/
