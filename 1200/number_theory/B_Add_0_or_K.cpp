8d078020eb28551066ba2283ccd7771c627d3c37
programmer-mursalin <programmer.mursalin016@gmail.com>
2025-08-30 15:36:09 +0600

    modular_aritgmatic

---
+++ 1200/number_theory/B_Add_0_or_K.cpp
@@ -0,0 +1,54 @@
+#include <bits/stdc++.h>
+// #include <ext/pb_ds/assoc_container.hpp>
+// #include <ext/pb_ds/tree_policy.hpp>
+using namespace std;
+// using namespace __gnu_pbds;
+#define ll long long
+#define gcd __gcd
+
+#define ALL(x) (x).begin(), (x).end()
+#define py cout << "YES\n";
+#define pm cout << "-1\n";
+#define pz cout << "0\n";
+#define pn cout << "NO\n";
+#define cheakmate return;
+const int N = 1e5 + 5;
+#define Mod 1000000009 + 7
+
+void solve()
+{
+
+    int n, k;
+    cin >> n >> k;
+    vector<ll> a(n);
+    for (ll &i : a)
+        cin >> i;
+    for (int i = 0; i < n; i++)
+    {
+        cout << a[i] + 1ll * (a[i] % (k + 1)) * k << ' ';
+    }
+    cout << '\n';
+}
+// sort(ALL(a),greater<int>());
+// int maxi=*max_element(a.begin(),a.end());
+//  int maxi = distance(a.begin(), max_element(a.begin(), a.end()));   // return max index
+
+signed main()
+{
+
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        solve();
+    }
+    return 0;
+}
+
+/*===============================================
+  :::::::::::::Author :Md.Mursalin:::::::::::::
+  ===============================================*/
