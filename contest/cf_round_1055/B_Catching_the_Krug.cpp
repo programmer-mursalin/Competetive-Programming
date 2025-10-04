0a2d05c4ea43b825443ac6ac6768d81c979cdae9
programmer-mursalin <programmer.mursalin016@gmail.com>
2025-10-04 22:57:27 +0600

    games

--- contest/cf_round_1055/B_Catching_the_Krug.cpp
+++ contest/cf_round_1055/B_Catching_the_Krug.cpp
@@ -1,26 +1,64 @@
 #include <bits/stdc++.h>
+// #include <ext/pb_ds/assoc_container.hpp>
+// #include <ext/pb_ds/tree_policy.hpp>
 using namespace std;
+// using namespace __gnu_pbds;
+#define int long long
+#define gcd __gcd
 
-int main()
+#define ALL(x) (x).begin(), (x).end()
+#define py cout << "YES\n";
+#define pm cout << "-1\n";
+#define pz cout << "0\n";
+#define pn cout << "NO\n";
+#define cheakmate return;
+const int N = 1e5 + 5;
+#define Mod 1000000009 + 7
+
+void solve()
 {
+
+    long long n, rK, cK, rD, cD;
+    cin >> n >> rK >> cK >> rD >> cD;
+
+    long long ans1 = 0, ans2 = 0;
+
+    if (rK < rD)
+        ans1 = rD;
+    else if (rK > rD)
+        ans1 = n - rD;
+    else
+        ans1 = 0;
+
+    if (cK < cD)
+        ans2 = cD;
+    else if (cK > cD)
+        ans2 = n - cD;
+    else
+        ans2 = 0;
+
+    cout << max(ans1, ans2) << "\n";
+}
+// sort(ALL(a),greater<int>());
+// int maxi=*max_element(a.begin(),a.end());
+//  int maxi = distance(a.begin(), max_element(a.begin(), a.end()));   // return max index
+
+signed main()
+{
+
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
+    cout.tie(nullptr);
 
     int t;
     cin >> t;
     while (t--)
     {
-        long long n, rK, cK, rD, cD;
-        cin >> n >> rK >> cK >> rD >> cD;
-
-        long long dx = abs(rK - rD);
-        long long dy = abs(cK - cD);
-
-        // Krug-এর survival time
-        long long survival = max(dx, dy);
-
-        cout << survival << "\n";
+        solve();
     }
-
     return 0;
 }
+
+/*===============================================
+  :::::::::::::Author :Md.Mursalin:::::::::::::
+  ===============================================*/
