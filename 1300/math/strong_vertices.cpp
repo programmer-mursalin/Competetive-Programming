825943f82855c6adb5a2be77f0493c95802a58ad
programmer-mursalin <programmer.mursalin016@gmail.com>
2025-10-10 11:26:39 +0600

    math

---
+++ 1300/math/strong_vertices.cpp
@@ -0,0 +1,78 @@
+#include <bits/stdc++.h>
+// #include <ext/pb_ds/assoc_container.hpp>
+// #include <ext/pb_ds/tree_policy.hpp>
+using namespace std;
+// using namespace __gnu_pbds;
+#define ll long long
+#define gcd __gcd
+
+#define ALL(x) (x).begin(), (x).end()
+#define py cout << "YES\n";
+#define pm cout << "-1\n";
+#define pz cout << "0\n";
+#define pn cout << "NO\n";
+#define cheakmate return;
+const int N = 1e5 + 5;
+#define Mod 1000000009 + 7
+
+void solve()
+{
+
+    ll n;
+    cin >> n;
+    ll arr[n];
+    for (ll i = 0; i < n; i++)
+    {
+        cin >> arr[i];
+    }
+
+    ll mx = -1e15;
+    for (ll j = 0; j < n; j++)
+    {
+        ll x;
+        cin >> x;
+        arr[j] -= x;
+        mx = max(mx, arr[j]);
+    }
+
+    vector<ll> ans;
+
+    for (ll j = 0; j < n; j++)
+    {
+        if (arr[j] == mx)
+        {
+            ans.push_back(j + 1);
+        }
+    }
+    sort(ans.begin(), ans.end());
+
+    cout << ans.size() << endl;
+    for (auto i : ans)
+    {
+        cout << i << " ";
+    }
+    cout << endl;
+}
+// sort(ALL(a),greater<int>());
+// int maxi=*max_element(a.begin(),a.end());
+//  int maxi = distance(a.begin(), max_element(a.begin(), a.end()));   // return max index
+
+signed main()
+{
+
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        solve();
+    }
+    return 0;
+}
+
+/*===============================================
+  :::::::::::::Author :Md.Mursalin:::::::::::::
+  ===============================================*/
