d894225792e9d52d1004fc4f3950564130e51df8
programmer-mursalin <programmer.mursalin016@gmail.com>
2025-11-18 23:33:51 +0600

    seive_term

---
+++ number_theory/divisor/all.cpp
@@ -0,0 +1,23 @@
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int d[104];
+int32_t main()
+{
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    int n = 100;
+    for (int i = 1; i <= n; i++)
+    {
+        for (int j = i; j <= n; j += i)
+        {
+            // d[j]++;
+            d[j] += i; // for sum of divisors
+        }
+    }
+
+    cout << d[6] << ' ';
+
+    return 0;
+}
