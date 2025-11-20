dcc671f824aec26c130cb4557eac7f1440f6130e
programmer-mursalin <programmer.mursalin016@gmail.com>
2025-11-20 22:44:49 +0600

    bitmask

---
+++ contest/cf_round_1018/C_2_Renako_Amaori_and_XOR_Game_hard_version.cpp
@@ -0,0 +1,93 @@
+#include <bits/stdc++.h>
+// #include <ext/pb_ds/assoc_container.hpp>
+// #include <ext/pb_ds/tree_policy.hpp>
+using namespace std;
+// using namespace __gnu_pbds;
+#define int long long
+#define gcd __gcd
+
+#define ALL(x) (x).begin(), (x).end()
+#define py cout << "YES\n";
+#define pm cout << "-1\n";
+#define pz cout << "0\n";
+#define pn cout << "NO\n";
+#define cheakmate return;
+const int N = 1e5 + 5;
+#define Mod 1000000009 + 7
+
+void solve()
+{
+
+    // 2d input
+    // vector<vector< int>> d(n, vector< int>(m));
+    int n;
+    cin >> n;
+    vector<int> a(n), b(n);
+
+    int cn1 = 0, cn2 = 0;
+    for (int i = 0; i < n; i++)
+    {
+        cin >> a[i];
+        cn1 ^= a[i];
+    }
+
+    for (int i = 0; i < n; i++)
+    {
+        cin >> b[i];
+        cn2 ^= b[i];
+    }
+
+    if ((cn1 ^ cn2) == 0)
+    {
+
+        cout << "Tie" << endl;
+        cheakmate
+    }
+    int pt = -1, pt2 = -1;
+    for (int i = 0; i < n; i++)
+    {
+        if (a[i] != b[i])
+            pt = i + 1;
+    }
+    for (int i = 0; i < n; i++)
+    {
+        if (a[i] != b[i])
+            pt2 = i + 1;
+    }
+
+    if (pt == -1)
+    {
+        cout << "Tie" << endl;
+    }
+    else if (pt & 1)
+    {
+
+        cout << "Ajisai" << endl;
+    }
+
+    else
+        cout << "Mai" << endl;
+}
+// sort(ALL(a),greater<int>());
+// int maxi=*max_element(a.begin(),a.end());
+//  int maxi = distance(a.begin(), max_element(a.begin(), a.end()));   // return max index
+
+signed main()
+{
+
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        solve();
+    }
+    return 0;
+}
+
+/*===============================================
+  :::::::::::::Author :Md.Mursalin:::::::::::::
+  ===============================================*/
